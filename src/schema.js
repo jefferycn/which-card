b152935b9d001b2001e1b3ab03c606aae98054fe
Postman Integration <integration@postman.com>
2021-07-29 14:14:59 -0700

    Postman updated which card 1.0.0 Commit reference: MDg0MzczMmMtODdhZi00ZmJhLThhYmItNjY0YTk0YTY5MWMw

--- src/schema.js
+++ src/schema.js
@@ -1,16 +1,42 @@
-schema {
-  query: Query
-  mutation: Mutation
-}
-type User {
-  id: String!
-  name: String
-}
-type Query {
-  user (id: String): User
-}
-type Mutation {
-  addUser (
-    # Name for the User item
-    name: String!): User!
-}
+scalar Date
+  type Card {
+    id: ID!
+    key: String!
+    name: String!
+  }
+
+  type Tag {
+    id: ID!
+    key: String!
+    name: String!
+  }
+
+  type Offer {
+    id: ID!
+    tag: Tag
+    card: Card
+    value: Int!
+    startDate: Date
+    endDate: Date
+  }
+
+  type Query {
+    offers: [Offer]!
+    getOffer(id: ID!): Offer!
+    getTag(key: String!): TagWithOffers!
+    getCard(key: String!): Card!
+  }
+
+  type TagWithOffers {
+    id: ID!
+    key: String!
+    name: String!
+    offers: [Offer]
+  }
+
+  type Mutation {
+    createTag(key: String!, name: String!): Tag!
+    createCard(key: String!, name: String!): Card!
+    createOffer(cardKey: String!, tagKey: String!, value: Int!, startDate: Date, endDate: Date): Offer!
+  }
+  
